833129cde595ddcad1d3e78caa25b19018f00868
GHub-PJ <peipengjun@gmail.com>
2020-07-06 10:23:19 +0800

    10:23

--- common/linux_update.py
+++ common/linux_update.py
@@ -5,7 +5,7 @@ import shutil
 if not os.path.exists('regular_expressions_local.json'):
     shutil.copy('regular_expressions_common.json', 'regular_expressions_local.json')
 
-with open('regular_expressions_local.json', 'r+') as fw:
+with open('regular_expressions_local.json', 'r+', encoding='utf8') as fw:
     contents = fw.readlines()
     my_str = contents[len(contents) -2]
     contents[len(contents) - 2] = my_str[:-1] + ',\n'
